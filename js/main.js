ec864a01be95497d30b157b814ed9d206af60cf9
Jessica Mello <jsvmello@gmail.com>
2020-09-09 22:27:27 -0300

    Includes the Hello World JS code

--- js/main.js
+++ js/main.js
@@ -7,3 +7,32 @@ function helloWorld() {
    }
    helloWorld();
    
+
+ // API MAP
+ let map;
+
+ function initMap() {
+   map = new google.maps.Map(document.getElementById("map"), {
+     center: { lat: -19.8837428, lng: -43.9341398 },
+     zoom: 8
+   });
+ }
+
+
+   /* API Drag and Drop
+
+   function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }*/
+
+
